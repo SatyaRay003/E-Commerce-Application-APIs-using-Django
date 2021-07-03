40c6ada7ddf1c06017cbe839f85a5af89956188a
SatyaRay003 <86600232+SatyaRay003@users.noreply.github.com>
2021-07-04 01:00:30 +0530

    Update urls.py

--- E_Commerce_DjangoAPI/E_Commerce_App/urls.py
+++ E_Commerce_DjangoAPI/E_Commerce_App/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
 
     url(r'^customer_order/$', views.customer_order_api),
-    url(r'^customer_order/([\w+]+)$', views.customer_order_api)
+    url(r'^customer_order/([\w+]+)$', views.customer_order_api),
+
+    url(r'^inventory_management/$', views.Inventory_management_api),
+    url(r'^inventory_management/([\w+]+)$', views.Inventory_management_api),
+
+    url(r'^purchase_order/$', views.Purchase_Order_api),
+    url(r'^purchase_order/([\w+]+)$', views.Purchase_Order_api)
 
 ]
